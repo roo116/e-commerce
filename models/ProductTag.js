776fc97be5a07782ba5f601998ed49e3b274311a
roo116 <roo116@nc.rr.com>
2022-07-13 02:56:43 -0400

    created some of the database table schemas

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,19 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    // id - integer, not null, primary key, auto-increment
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+
+    // product_id - integer, fk to product(id)
+   product_id: {
+    type:DataTypes.INTEGER
+    }
+    // tag id - integer, fk to tag(id)
   },
   {
     sequelize,
--- models/index.js
+++ models/index.js
@@ -4,9 +4,24 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Products belongs To Category
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
+
+Product.belongsTo(Category,{
+  foreignKey:'category_id',
+  onDelete: 'cascade'
+});
+
+Product.hasMany(Tag,{})
+Product.belongsTo(Tag,{})
+
+Tag.hasMany(Product,{})
+Tag.belongsTo(Product,{})
+
 
 // Products belongToMany Tags (through ProductTag)
 
