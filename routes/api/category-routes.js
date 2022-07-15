637a9d4d8763f735051fdc7e033699075ddbf7f6
roo116 <roo116@nc.rr.com>
2022-07-14 22:24:58 -0400

    completed catagory get route with associated products

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,25 +4,45 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-  // find all categories
+  Category.findAll(
+    {
+      include: {
+        model: Product,
+        attributes: ['product_name']
+      }
+    }
+  ).then(dbCatData=> res.json(dbCatData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+
   // be sure to include its associated Products
 });
 
 router.get('/:id', (req, res) => {
+  Category.findOne({
+    where: {
+      attribute: req.params.id
+    }
+  }).then(dbCatData)
   // find one category by its `id` value
   // be sure to include its associated Products
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create()
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.put()
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy();
 });
 
 module.exports = router;
