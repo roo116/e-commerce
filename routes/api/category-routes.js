333a03426583cd77ab475c625f9a65479088a51d
roo116 <roo116@nc.rr.com>
2022-07-16 04:53:22 -0400

    created api route to delete category by id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -79,7 +79,20 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
-  Category.destroy();
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(dbCatData =>{
+    if(!dbCatData) {
+      res.status(404).json({message: 'ID not found'});
+      return;
+    }
+    res.json(dbCatData);
+  }).catch(err=>{
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
