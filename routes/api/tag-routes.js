ae93aa96f6e6dd6ce989d2e28b61c87ba492bf2c
roo116 <roo116@nc.rr.com>
2022-07-16 03:45:00 -0400

    completed tag findAll api route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,10 +1,27 @@
 const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
+const { findAll } = require('../../models/Product');
 
 // The `/api/tags` endpoint
 
 router.get('/', (req, res) => {
   // find all tags
+  Tag.findAll({
+    attributes: [
+'id',
+'tag_name'
+    ],
+   include: {
+    model: Product,
+    attributes: ['product_name'],
+    through: ProductTag,
+    as: 'products'
+    }
+  }).then(dbTagData => res.json(dbTagData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err)
+  })
   // be sure to include its associated Product data
 });
 
